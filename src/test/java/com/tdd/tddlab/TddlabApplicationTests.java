43788f6efbe59e9d70c2ec4118bd388bf2c02244
Dioney Becker <Dioney.Becker@sprint.com>
2021-06-10 13:52:10 -0300

    working code all tests pass

--- src/test/java/com/tdd/tddlab/TddlabApplicationTests.java
+++ src/test/java/com/tdd/tddlab/TddlabApplicationTests.java
@@ -10,36 +10,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 //@SpringBootTest
 class TddlabApplicationTests {
 
-	@DisplayName(value = "canary test")
-	@Test
-	void contextLoads() {
-		assertEquals(true, true);
-	}
+    @DisplayName(value = "canary test")
+    @Test
+    void contextLoads() {
+        assertEquals(true, true);
+    }
 
-	@Nested
-	@DisplayName(value = "fahrenheit to celsius converter behaves as follows")
-	class fahrenheitToCelsius {
+    @Nested
+    @DisplayName(value = "fahrenheit to celsius converter behaves as follows")
+    class fahrenheitToCelsius {
 
-		@Test
-		@DisplayName(value = "212 degrees fahrenheit is 100 degrees celius")
-		void convert_Fahrenheit_To_Celsius_212_to_100() {
-			assertEquals(100, fahrenheit2Celsius(212));
-		}
+        @Test
+        @DisplayName(value = "212 degrees fahrenheit is 100 degrees celius")
+        void convert_Fahrenheit_To_Celsius_212_to_100() {
+            assertEquals(100, fahrenheit2Celsius(212));
+        }
 
-		@Test
-		@DisplayName(value = "32 degrees fahrenheit is 0 degrees celius")
-		void convert_Fahrenheit_To_Celsius_32_to_0() {
-			assertEquals( 0, fahrenheit2Celsius(32));
-		}
+        @Test
+        @DisplayName(value = "32 degrees fahrenheit is 0 degrees celius")
+        void convert_Fahrenheit_To_Celsius_32_to_0() {
+            assertEquals(0, fahrenheit2Celsius(32));
+        }
 
-		@Test
-		@DisplayName(value = "50 degrees fahrenheit is 10 degrees celius")
-		void convert_Fahrenheit_To_Celsius_50_to_10() {
-		}
-	}
+        @Test
+        @DisplayName(value = "50 degrees fahrenheit is 10 degrees celius")
+        void convert_Fahrenheit_To_Celsius_50_to_10() {
+            assertEquals(10, fahrenheit2Celsius(50));
+        }
 
-	private int fahrenheit2Celsius(int fahrenheit) {
-		if (fahrenheit == 32) return 0;
-		return 100;
-	}
+        @Test
+        @DisplayName(value = "-22 degrees fahrenheit is -30 degrees celius")
+        void convert_Fahrenheit_To_Celsius_minus22_to_minus30() {
+            assertEquals(-30, fahrenheit2Celsius(-22));
+        }
+    }
+
+    private int fahrenheit2Celsius(int fahrenheit) {
+        return ((fahrenheit - 32) * 5 / 9);
+    }
 }
