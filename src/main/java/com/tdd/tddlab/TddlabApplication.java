230bf33ae0c43e0b51bf34134d935f7d624457e6
Dioney Becker <Dioney.Becker@sprint.com>
2021-06-10 11:48:16 -0300

    creted the test suite

---
+++ src/main/java/com/tdd/tddlab/TddlabApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.tddlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddlabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddlabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tdd/tddlab/TddlabApplicationTests.java
@@ -0,0 +1,40 @@
+package com.tdd.tddlab;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+//@SpringBootTest
+class TddlabApplicationTests {
+
+	@DisplayName(value = "canary test")
+	@Test
+	void contextLoads() {
+		assertEquals(true, true);
+	}
+
+	@Nested
+	@DisplayName(value = "fahrenheit to celsius converter behaves as follows")
+	class fahrenheitToCelsius {
+
+		@Test
+		@DisplayName(value = "212 degrees fahrenheit is 100 degrees celius")
+		void convert_Fahrenheit_To_Celsius_212_to_100() {
+		}
+
+		@Test
+		@DisplayName(value = "32 degrees fahrenheit is 0 degrees celius")
+		void convert_Fahrenheit_To_Celsius_32_to_0() {
+		}
+
+		@Test
+		@DisplayName(value = "50 degrees fahrenheit is 10 degrees celius")
+		void convert_Fahrenheit_To_Celsius_50_to_10() {
+		}
+	}
+
+
+}
